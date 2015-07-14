bc2e5f91132f44338462f55bf3bbb8f7f6f675ea
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2015-07-14 14:47:33 -0500

    Create proyect

---
+++ WoWGoblin/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WoWGoblin {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Item item = new Item();
+            string json = JsonConvert.SerializeObject(item);
+            string url = "http://api.tradeskillmaster.com/sample_auction_data.json";
+            Item deserializedProduct = JsonConvert.DeserializeObject<Item>(json);
+            label1.Text = deserializedProduct.itemName;
+            using (var w = new WebClient()) {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try {
+                    json_data = w.DownloadString(url);
+                } catch (Exception error) {
+                    MessageBox.Show(error.Message);
+                }
+                
+            }
+        }
+    }
+}
---
+++ WoWGoblin/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWGoblin {
+    class Item {
+        public string itemName { get; set; }
+        public string marketValue { get; set; }
+        public string minBuyout { get; set; }
+        public string historicalPrice { get; set; }
+        public string quantity { get; set; }
+        public string globalMarketValue { get; set; }
+        public string globalMinBuyout { get; set; }
+        public string globalHistoricalPrice { get; set; }
+        public string globalQuantity { get; set; }
+        public string globalSalePrice { get; set; }
+    }
+}
