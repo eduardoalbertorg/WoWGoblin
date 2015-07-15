3d5d9a181b86e4cde1999790e5b34e0be9097217
Eduardo Alberto Rodriguez Garcia <eduardoalbertorg@gmail.com>
2015-07-15 12:33:57 -0500

    Remake UI

--- WoWGoblin/Form1.cs
+++ WoWGoblin/Form1.cs
@@ -17,20 +17,7 @@ namespace WoWGoblin {
             InitializeComponent();
         }
 
-        private static T _download_serialized_json_data<T>(string url) where T : new() {
-            using (var w = new WebClient()) {
-                var json_data = string.Empty;
-                // attempt to download JSON data as a string
-                try {
-                    json_data = w.DownloadString(url);
-                } catch (Exception) { }
-                // if string with JSON data is not empty, deserialize it to class and return its instance 
-                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
-            }
-        }
-
-
-        private void button1_Click(object sender, EventArgs e) {
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e) {
             string url = "http://api.tradeskillmaster.com/sample_auction_data.json";
             using (var w = new WebClient()) {
                 var json_data = string.Empty;
@@ -45,7 +32,6 @@ namespace WoWGoblin {
                     dgvItems.Rows.Add(item.Value.itemName);
                 }
             }
-
         }
     }
 }
